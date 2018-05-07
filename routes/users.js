dda58c6bc667aaec6c6d08c2fb7e749c46ad07bc
MrDwina <diwina1993@hotmail.com>
2018-05-07 19:39:21 +0100

    fixed user model path issue

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var path = require('path');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var User = require(path.join(__dirname, '/../models/user'));
+var User = path.dirname(require.main.filename) + '/../models/user';
 const ensureAuthenticated = require('../login/ensureAuthenticated');
 
 /* GET users listing. */
